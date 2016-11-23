43637155d767cfa32268791f06b977a8d5486c17
Администратор <gotti280985@gmail.com>
2016-11-23 10:36:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Администратор on 23.11.2016.
+ */
+public class Main {
+    //Простой класс, чисто для проверки
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
