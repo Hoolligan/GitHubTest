4e6ca1ca9ea909073620fdfb0daf016b522b08e0
Администратор <gotti280985@gmail.com>
2016-11-23 10:41:03 +0300

    Добавили одну строку с изменениями

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
     //Простой класс, чисто для проверки
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        //Теперь внесём чуть изменений.
+        System.out.println("Some changes");
     }
 }
